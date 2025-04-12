a42eaf95eb2812c2733a90f22c7727585435059f
LCSOGthb <185141600+LCSOGthb@users.noreply.github.com>
2025-04-12 12:33:08 +0800

    VSCode (#6)

---
+++ debug.js
@@ -0,0 +1,4 @@
+// Ensure 'debug' is declared only once
+const debug = (message) => {
+    console.log(message);
+};
---
+++ dialog.js
@@ -0,0 +1,6 @@
+
+
+
+
+
+// ...existing code...};    // ...existing code...const Dialog = {
