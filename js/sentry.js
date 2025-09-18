b1a3af636e57476e6aa2b52f5456ee928b27f0b6
LCSOGthb <185141600+LCSOGthb@users.noreply.github.com>
2025-09-18 03:57:54 +0000

    Changed the DSN

--- js/sentry.js
+++ js/sentry.js
@@ -1,5 +1,21 @@
 Sentry.init({
-  dsn: "https://3410730c33a0ed28fc51370949258ba8@o4508406269995008.ingest.us.sentry.io/4508406275608576",
+  dsn: "https://c38250fd2a7fdb30e16b4673276ff3de@o4509151884148736.ingest.us.sentry.io/4510038332080128",
   integrations: [new Sentry.BrowserTracing()],
-  tracesSampleRate: 1.0,
+  tracesSampleRate: 1.0, // 100% performance data (lower later if too much)
 });
+
+// Optional: track page visits
+Sentry.addBreadcrumb({
+  category: "navigation",
+  message: `Visited ${window.location.pathname}`,
+  level: "info",
+});
+
+// Test error (you can remove later)
+setTimeout(() => {
+  try {
+    myUndefinedFunction(); // should trigger an error in Sentry
+  } catch (err) {
+    Sentry.captureException(err);
+  }
+}, 2000);
