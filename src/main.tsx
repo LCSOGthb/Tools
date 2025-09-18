ce638fbe4595bf77c3b055f9121eef0503a0f711
LCSOGthb <185141600+LCSOGthb@users.noreply.github.com>
2025-09-18 11:02:46 +0000

    Fixed Sentry.io not reciving events

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
+import "./sentry"; // ⚠️ must be first
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import * as Sentry from "@sentry/react";
-import "./sentry";
+import { ErrorBoundary } from "@sentry/react";
 
 const container = document.getElementById("app")!;
 const root = createRoot(container);
-
+root.render(<App />);
 root.render(
-  <Sentry.ErrorBoundary fallback={<h2>Something went wrong.</h2>}>
+  <ErrorBoundary fallback={<h2>Something went wrong.</h2>}>
     <App />
-  </Sentry.ErrorBoundary>
+  </ErrorBoundary>
 );
