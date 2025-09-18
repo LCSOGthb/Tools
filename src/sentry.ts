7326d10a18ef42d40b080362a2fc66044b737edb
LCSOGthb <185141600+LCSOGthb@users.noreply.github.com>
2025-09-18 23:55:43 +0800

    Fixed sentry not working

--- src/sentry.ts
+++ src/sentry.ts
@@ -4,13 +4,11 @@ import { BrowserTracing } from "@sentry/tracing";
 Sentry.init({
   dsn: "https://de284553b9b4c7a3f7bcd9786eb802f2@o4509151884148736.ingest.us.sentry.io/4510040067735557",
   sendDefaultPii: true,
-  integrations: [
-    new BrowserTracing(), // React + Browser tracing
-    new Sentry.Replay(),  // Session replay
-  ],
+  integrations: [new BrowserTracing(), new Sentry.Replay()],
   tracesSampleRate: 1.0,
-  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
   replaysSessionSampleRate: 0.1,
   replaysOnErrorSampleRate: 1.0,
   enableLogs: true,
 });
+
+Sentry.captureException(new Error("Automatic test error on page load"));
