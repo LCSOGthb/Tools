ad984ccc05b2ee448e4f58d950a5c5805dd7452e
LCSOGthb <185141600+LCSOGthb@users.noreply.github.com>
2025-09-18 05:01:50 +0000

    Changed sentry working strucure and modifed the settings

--- js/sentry.js
+++ js/sentry.js
@@ -1,7 +1,12 @@
 Sentry.init({
   dsn: "https://c38250fd2a7fdb30e16b4673276ff3de@o4509151884148736.ingest.us.sentry.io/4510038332080128",
-  integrations: [Sentry.browserTracingIntegration()],
-  tracesSampleRate: 1.0, // 100% performance data (lower later if too much)
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0
 });
 
 Sentry.addBreadcrumb({
