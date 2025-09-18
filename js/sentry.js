12998d68a7197a46d74888d3586d1cae9f064c31
LCSOGthb <185141600+LCSOGthb@users.noreply.github.com>
2025-09-18 04:46:36 +0000

    Improved sentry.js strucure

--- js/sentry.js
+++ js/sentry.js
@@ -1,10 +1,9 @@
 Sentry.init({
   dsn: "https://c38250fd2a7fdb30e16b4673276ff3de@o4509151884148736.ingest.us.sentry.io/4510038332080128",
-  integrations: [],
+  integrations: [Sentry.browserTracingIntegration()],
   tracesSampleRate: 1.0, // 100% performance data (lower later if too much)
 });
 
-// Optional: track page visits
 Sentry.addBreadcrumb({
   category: "navigation",
   message: `Visited ${window.location.pathname}`,
