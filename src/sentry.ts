5f323c7b17fbbe213242289f80934029008c1510
LCSOGthb <185141600+LCSOGthb@users.noreply.github.com>
2025-09-19 22:10:41 +0800

    Fixed false error reported on sentry.io

--- src/sentry.ts
+++ src/sentry.ts
@@ -1,14 +1,10 @@
 import * as Sentry from "@sentry/react";
-import { BrowserTracing } from "@sentry/tracing";
-import { Replay } from "@sentry/replay";
 
 Sentry.init({
   dsn: "https://de284553b9b4c7a3f7bcd9786eb802f2@o4509151884148736.ingest.us.sentry.io/4510040067735557",
   integrations: [
     Sentry.browserTracingIntegration(),
     Sentry.replayIntegration(),
-    new BrowserTracing(), 
-    new Replay(),
   ],
   tracesSampleRate: 1.0,
   replaysSessionSampleRate: 0.1,
