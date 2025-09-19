70e5cfd2193ccc71a3dd4a152b248c3d814c7199
LCSOGthb <185141600+LCSOGthb@users.noreply.github.com>
2025-09-19 22:45:26 +0800

    Add Codecov bundler plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from "vite";
+import { codecovVitePlugin } from "@codecov/vite-plugin";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    codecovVitePlugin({
+      enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
+      bundleName: "Tools",
+      uploadToken: process.env.CODECOV_TOKEN,
+    }),
+  ],
 });
