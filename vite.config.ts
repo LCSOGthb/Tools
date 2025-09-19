a19b8c72df9e48fa758c8bbf3b57b731b4a298db
LCSOGthb <185141600+LCSOGthb@users.noreply.github.com>
2025-09-19 23:20:38 +0800

    Removed codecov support

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,6 @@
 import { defineConfig } from "vite";
-import { codecovVitePlugin } from "@codecov/vite-plugin";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    codecovVitePlugin({
-      enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
-      bundleName: "Tools",
-      uploadToken: process.env.CODECOV_TOKEN,
-      telemetry: false
-    }),
-  ],
+  plugins: [react()],
 });
